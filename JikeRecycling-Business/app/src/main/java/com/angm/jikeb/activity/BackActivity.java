cd8cedd1894b988508eafb5dbc0d0b6103c4aead
shd <837957997@qq.com>
2016-08-03 22:27:00 +0800

    dfd

--- JikeRecycling-Business/app/src/main/java/com/angm/jikeb/activity/BackActivity.java
+++ JikeRecycling-Business/app/src/main/java/com/angm/jikeb/activity/BackActivity.java
@@ -2,31 +2,43 @@ package com.angm.jikeb.activity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.angm.jikeb.R;
+
+import butterknife.Bind;
 
 /**
  * Created by shd on 16-8-3.
+ * 用于 带返回按钮的activity
  */
-public class BackActivity extends BaseActivity {
+public abstract class BackActivity extends BaseActivity {
 
+
+    @Bind(R.id.tv_toolbar_center)
+    TextView tvToolbarCenter;
+    @Bind(R.id.iv_back)
+    ImageView ivBack;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    private View toolbarView;
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
-
-     }
-
-    @Override
-    public int getLayout() {
-        return 0;
+        toolbarView = LayoutInflater.from(this).inflate(R.layout.activity_back, null);
+        initToolBar();
     }
 
-    @Override
-    public void initView() {
-
+    // TODO: 2016-08-03    对 toolbar 进行重新赋值
+    public void initToolBar() {
+        tvToolbarCenter.setText(getTitileStr());
     }
 
-    @Override
-    public void initData() {
-
-    }
+     // TODO: 2016-08-03   重新进行赋值
+     abstract String getTitileStr();
 }

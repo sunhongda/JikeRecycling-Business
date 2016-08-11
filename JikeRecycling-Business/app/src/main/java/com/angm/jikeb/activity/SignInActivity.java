ae6f245e341af51de9bc9cfc6e99073a1254463b
unknown <>
2016-08-11 11:25:31 +0800

    注释

--- JikeRecycling-Business/app/src/main/java/com/angm/jikeb/activity/SignInActivity.java
+++ JikeRecycling-Business/app/src/main/java/com/angm/jikeb/activity/SignInActivity.java
@@ -2,10 +2,10 @@ package com.angm.jikeb.activity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.angm.jikeb.R;
 import com.angm.jikeb.base.BaseActivity;
@@ -16,22 +16,22 @@ import butterknife.Bind;
 //登录
 public class SignInActivity extends BaseActivity {
 
+
     @Bind(R.id.signin_mobile_edittext)
     EditText signinMobileEdittext;
-    @Bind(R.id.signin_verify_button)
-    Button signinVerifyButton;
-    @Bind(R.id.signin_captcha_edittext)
-    EditText signinCaptchaEdittext;
+    @Bind(R.id.signin_pwd_edittext)
+    EditText signinPwdEdittext;
+    @Bind(R.id.signin_chechkbox)
+    CheckBox signinChechkbox;
     @Bind(R.id.signin_start_button)
     Button signinStartButton;
-    @Bind(R.id.tv)
-    TextView tv;
-    @Bind(R.id.aiplay_view)
-    ImageView aiplayView;
+    @Bind(R.id.toolbar_signin_activity)
+    Toolbar toolbarTop;
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        initToolbar();
     }
 
     @Override
@@ -42,6 +42,10 @@ public class SignInActivity extends BaseActivity {
     @Override
     public void initView() {
 
+    }
+    private void initToolbar() {
+        toolbarTop.setTitle("登录");
+        toolbarTop.setNavigationIcon(null);
     }
 
     @Override

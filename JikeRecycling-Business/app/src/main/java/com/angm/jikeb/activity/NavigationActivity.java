6671482f627fc921d3a963b7e9e6577186e4dcd1
unknown <>
2016-08-11 21:40:00 +0800

    注释

---
+++ JikeRecycling-Business/app/src/main/java/com/angm/jikeb/activity/NavigationActivity.java
@@ -0,0 +1,145 @@
+package com.angm.jikeb.activity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.angm.jikeb.R;
+
+import cn.taurusxi.guidebackgroundcoloranimation.library.ColorAnimationView;
+
+public class NavigationActivity extends AppCompatActivity {
+    private static final int[] resource = new int[]{R.mipmap.welcome1, R.mipmap.welcome4,
+            R.mipmap.welcome3, R.mipmap.welcome4};
+    private ViewPager viewPager;
+    private int currentItem;
+    private SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+        /*存储第一次进入显示*/
+        preferences = getSharedPreferences("navigation", MODE_PRIVATE);
+        boolean flag = preferences.getBoolean("flag", true);
+        if (!flag) {
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+        }
+        MyFragmentStatePager adpter = new MyFragmentStatePager(getSupportFragmentManager());
+        ColorAnimationView colorAnimationView = (ColorAnimationView) findViewById(R.id.ColorAnimationView);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(adpter);
+        /*监听*/
+        /*动画适配器*/
+        colorAnimationView.setmViewPager(viewPager, resource.length);
+        colorAnimationView.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                currentItem = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+
+        });
+        viewPager.setOnTouchListener(new View.OnTouchListener() {
+            float startX;
+            float startY;
+            float endX;
+            float endY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        startX = event.getX();
+                        startY = event.getY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        endX = event.getX();
+                        endY = event.getY();
+                        WindowManager windowManager = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+                        //获取屏幕的宽度
+                        Point size = new Point();
+                        windowManager.getDefaultDisplay().getSize(size);
+                        int width = size.x;
+                        //首先要确定的是，是否到了最后一页，然后判断是否向左滑动，并且滑动距离是否符合，我这里的判断距离是屏幕宽度的4分之一（这里可以适当控制）
+                        if (currentItem == (resource.length - 1) && startX - endX > 0 && startX - endX >= (width / 4)) {
+                            Log.i("zzz", "进入了触摸");
+                            goToMainActivity();
+                            overridePendingTransition(R.anim.slide_in_right,R.anim.slide_in_left);
+                        }
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+
+    private void goToMainActivity() {
+        startActivity(new Intent(this, SignInActivity.class));
+        finish();
+
+        SharedPreferences.Editor ed = preferences.edit();
+        ed.putBoolean("flag", false);
+        ed.commit();
+    }
+
+    public class MyFragmentStatePager
+            extends FragmentStatePagerAdapter {
+
+        public MyFragmentStatePager(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new MyFragment(position);
+        }
+
+        @Override
+        public int getCount() {
+            return resource.length;
+        }
+    }
+
+    @SuppressLint("ValidFragment")
+    public class MyFragment extends Fragment {
+        private int position;
+
+        public MyFragment(int position) {
+            this.position = position;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setImageResource(resource[position]);
+            return imageView;
+        }
+    }
+}
--- JikeRecycling-Business/app/src/main/java/com/angm/jikeb/activity/SignInActivity.java
+++ JikeRecycling-Business/app/src/main/java/com/angm/jikeb/activity/SignInActivity.java
@@ -2,15 +2,40 @@ package com.angm.jikeb.activity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.angm.jikeb.R;
 import com.angm.jikeb.base.BaseActivity;
+import com.angm.jikeb.util.Alert;
+import com.angm.jikeb.util.EncryptUtil;
+import com.angm.jikeb.util.OkHttpUtils;
+import com.angm.jikeb.util.SignUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.Bind;
 
 
 //登录
 public class SignInActivity extends BaseActivity {
 
 
+    @Bind(R.id.signin_chechkbox)
+    CheckBox signinChechkbox;
+    @Bind(R.id.signin_start_button)
+    Button signinStartButton;
+    @Bind(R.id.signin_mobile_edittext)
+    EditText signinMobileEdittext;
+    @Bind(R.id.signin_pwd_edittext)
+    EditText signinPwdEdittext;
+    private String path = "http://115.28.242.49:8080/getVerCode.do";
+    private Map<String, Object> map = new HashMap<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
@@ -34,6 +59,58 @@ public class SignInActivity extends BaseActivity {
     @Override
     public void initData() {
 
+        signinStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mobile = signinMobileEdittext.getText().toString().trim();/*手机号*/
+                String pwd = signinPwdEdittext.getText().toString().trim();/*密码*/
+                boolean mobileNO = isMobileNO(mobile);/*手机号正则表达式*/
+                if (mobileNO) {
+                    if (TextUtils.isEmpty(pwd)) {
+                        Toast.makeText(SignInActivity.this, R.string.sign_in_pwd, Toast.LENGTH_SHORT).show();
+                    } else {
+                        if (pwd.length() < 6 || pwd.length() > 20) {
+                            Toast.makeText(SignInActivity.this, R.string.sign_in_pwd_count, Toast.LENGTH_SHORT).show();
+                        } else {
+                            if (!signinChechkbox.isChecked()) {
+                                Toast.makeText(SignInActivity.this, R.string.sign_in_check, Toast.LENGTH_SHORT).show();
+                            } else {
+                                Alert.showDialog(SignInActivity.this);
+                                String encryMobile = EncryptUtil.getEncryptString(mobile);
+                                /*签名*/
+                                String[] strPath = new String[]{path};
+                                String sign = SignUtil.getSign(strPath);
+                                map.put("mobile", encryMobile);
+                                OkHttpUtils.getInstance(new OkHttpUtils.HttpCallBackListener() {
+                                    @Override
+                                    public void response(String result) {
+                                        android.util.Log.d("zzz", "result+" + result);
+                                        Alert.mProgressDialog.dismiss();
+                                    }
+                                }).POST_DATA(sign, map);
+                            }
+                        }
+                    }
+                } else {
+                    Toast.makeText(SignInActivity.this, R.string.sign_in_mobile_format, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * 验证手机格式
+     */
+    public static boolean isMobileNO(String mobiles) {
+    /*
+    移动：134、135、136、137、138、139、150、151、157(TD)、158、159、187、188
+    联通：130、131、132、152、155、156、185、186
+    电信：133、153、180、189、（1349卫通）
+    总结起来就是第一位必定为1，第二位必定为3或5或8，其他位置的可以为0-9
+    */
+        String telRegex = "[1][358]\\d{9}";//"[1]"代表第1位为数字1，"[358]"代表第二位可以为3、5、8中的一个，"\\d{9}"代表后面是可以是0～9的数字，有9位。
+        if (TextUtils.isEmpty(mobiles)) return false;
+        else return mobiles.matches(telRegex);
     }
 }
 

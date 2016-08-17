d24092a8f4ef98c07eee15c107153dc056448800
sunhongda <shdshd6@163.com>
2016-08-17 11:40:58 +0800

    修改点击颜色

--- JikeRecycling-Business/app/src/main/java/com/angm/jikeb/activity/H5PayDemoActivity.java
+++ JikeRecycling-Business/app/src/main/java/com/angm/jikeb/activity/H5PayDemoActivity.java
@@ -21,130 +21,130 @@ import com.alipay.sdk.util.H5PayResultModel;
 
 public class H5PayDemoActivity extends Activity {
 
-	private WebView mWebView;
+    private WebView mWebView;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Bundle extras = null;
-		try {
-			extras = getIntent().getExtras();
-		} catch (Exception e) {
-			finish();
-			return;
-		}
-		if (extras == null) {
-			finish();
-			return;
-		}
-		String url = null;
-		try {
-			url = extras.getString("url");
-		} catch (Exception e) {
-			finish();
-			return;
-		}
-		if (TextUtils.isEmpty(url)) {
-			// 测试H5支付，必须设置要打开的url网站
-			new AlertDialog.Builder(H5PayDemoActivity.this).setTitle("警告")
-					.setMessage("必须配置需要打开的url 站点，请在PayDemoActivity类的h5Pay中配置")
-					.setPositiveButton("确定", new OnClickListener() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle extras = null;
+        try {
+            extras = getIntent().getExtras();
+        } catch (Exception e) {
+            finish();
+            return;
+        }
+        if (extras == null) {
+            finish();
+            return;
+        }
+        String url = null;
+        try {
+            url = extras.getString("url");
+        } catch (Exception e) {
+            finish();
+            return;
+        }
+        if (TextUtils.isEmpty(url)) {
+            // 测试H5支付，必须设置要打开的url网站
+            new AlertDialog.Builder(H5PayDemoActivity.this).setTitle("警告")
+                    .setMessage("必须配置需要打开的url 站点，请在PayDemoActivity类的h5Pay中配置")
+                    .setPositiveButton("确定", new OnClickListener() {
 
-						@Override
-						public void onClick(DialogInterface arg0, int arg1) {
-							finish();
-						}
-					}).show();
+                        @Override
+                        public void onClick(DialogInterface arg0, int arg1) {
+                            finish();
+                        }
+                    }).show();
 
-		}
-		super.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		LinearLayout layout = new LinearLayout(getApplicationContext());
-		LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-		layout.setOrientation(LinearLayout.VERTICAL);
-		setContentView(layout, params);
+        }
+        super.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        LinearLayout layout = new LinearLayout(getApplicationContext());
+        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(layout, params);
 
-		mWebView = new WebView(getApplicationContext());
-		params.weight = 1;
-		mWebView.setVisibility(View.VISIBLE);
-		layout.addView(mWebView, params);
+        mWebView = new WebView(getApplicationContext());
+        params.weight = 1;
+        mWebView.setVisibility(View.VISIBLE);
+        layout.addView(mWebView, params);
 
-		WebSettings settings = mWebView.getSettings();
-		settings.setRenderPriority(RenderPriority.HIGH);
-		settings.setSupportMultipleWindows(true);
-		settings.setJavaScriptEnabled(true);
-		settings.setSavePassword(false);
-		settings.setJavaScriptCanOpenWindowsAutomatically(true);
-		settings.setMinimumFontSize(settings.getMinimumFontSize() + 8);
-		settings.setAllowFileAccess(false);
-		settings.setTextSize(WebSettings.TextSize.NORMAL);
-		mWebView.setVerticalScrollbarOverlay(true);
-		mWebView.setWebViewClient(new MyWebViewClient());
-		mWebView.loadUrl(url);
-	}
+        WebSettings settings = mWebView.getSettings();
+        settings.setRenderPriority(RenderPriority.HIGH);
+        settings.setSupportMultipleWindows(true);
+        settings.setJavaScriptEnabled(true);
+        settings.setSavePassword(false);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setMinimumFontSize(settings.getMinimumFontSize() + 8);
+        settings.setAllowFileAccess(false);
+        settings.setTextSize(WebSettings.TextSize.NORMAL);
+        mWebView.setVerticalScrollbarOverlay(true);
+        mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.loadUrl(url);
+    }
 
-	@Override
-	public void onBackPressed() {
-		if (mWebView.canGoBack()) {
-			mWebView.goBack();
-		} else {
-			finish();
-		}
-	}
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            finish();
+        }
+    }
 
-	@Override
-	public void finish() {
-		super.finish();
-	}
+    @Override
+    public void finish() {
+        super.finish();
+    }
 
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-		super.onConfigurationChanged(newConfig);
-	}
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
 
-	private class MyWebViewClient extends WebViewClient {
+    private class MyWebViewClient extends WebViewClient {
 
-		@Override
-		public boolean shouldOverrideUrlLoading(final WebView view, String url) {
-			if (!(url.startsWith("http") || url.startsWith("https"))) {
-				return true;
-			}
+        @Override
+        public boolean shouldOverrideUrlLoading(final WebView view, String url) {
+            if (!(url.startsWith("http") || url.startsWith("https"))) {
+                return true;
+            }
 
-			final PayTask task = new PayTask(H5PayDemoActivity.this);
-			final String ex = task.fetchOrderInfoFromH5PayUrl(url);
-			if (!TextUtils.isEmpty(ex)) {
-				System.out.println("paytask:::::" + url);
-				new Thread(new Runnable() {
-					public void run() {
-						System.out.println("payTask:::" + ex);
-						final H5PayResultModel result = task.h5Pay(ex, true);
-						if (!TextUtils.isEmpty(result.getReturnUrl())) {
-							H5PayDemoActivity.this.runOnUiThread(new Runnable() {
-								
-								@Override
-								public void run() {
-									view.loadUrl(result.getReturnUrl());
-								}
-							});
-						}
-					}
-				}).start();
-			} else {
-				view.loadUrl(url);
-			}
-			return true;
-		}
-	}
+            final PayTask task = new PayTask(H5PayDemoActivity.this);
+            final String ex = task.fetchOrderInfoFromH5PayUrl(url);
+            if (!TextUtils.isEmpty(ex)) {
+                System.out.println("paytask:::::" + url);
+                new Thread(new Runnable() {
+                    public void run() {
+                        System.out.println("payTask:::" + ex);
+                        final H5PayResultModel result = task.h5Pay(ex, true);
+                        if (!TextUtils.isEmpty(result.getReturnUrl())) {
+                            H5PayDemoActivity.this.runOnUiThread(new Runnable() {
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		if (mWebView != null) {
-			mWebView.removeAllViews();
-			try {
-				mWebView.destroy();
-			} catch (Throwable t) {
-			}
-			mWebView = null;
-		}
-	}
+                                @Override
+                                public void run() {
+                                    view.loadUrl(result.getReturnUrl());
+                                }
+                            });
+                        }
+                    }
+                }).start();
+            } else {
+                view.loadUrl(url);
+            }
+            return true;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mWebView != null) {
+            mWebView.removeAllViews();
+            try {
+                mWebView.destroy();
+            } catch (Throwable t) {
+            }
+            mWebView = null;
+        }
+    }
 }

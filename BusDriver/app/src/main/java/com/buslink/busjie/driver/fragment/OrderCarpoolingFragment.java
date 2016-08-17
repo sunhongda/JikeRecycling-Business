86ce87c17e00c2f0dee444b7a19382401ef4d7b0
sunhongda <shdshd6@163.com>
2016-08-17 17:01:48 +0800

    delete busdriver

--- BusDriver/app/src/main/java/com/buslink/busjie/driver/fragment/OrderCarpoolingFragment.java
+++
@@ -1,9 +0,0 @@
-package com.buslink.busjie.driver.fragment;
-
-/**
- * Created by Administrator on 2016/2/20.
- */
-public class OrderCarpoolingFragment extends BaseFragment {
-
-
-}
--- BusDriver/app/src/main/java/com/buslink/busjie/driver/view/InputListener.java
+++
@@ -1,7 +0,0 @@
-package com.buslink.busjie.driver.view;
-
-public interface InputListener {
-    public void onErr();
-    public void onCancle();
-    public void onTrue(String tv);
-}
